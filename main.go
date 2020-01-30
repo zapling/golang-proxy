592c83d48d7b48d6b7455a46365eef156aeea98d
Andreas Palm <andreaspalm98@gmail.com>
2020-01-30 14:09:34 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+func printUsage() {
+	text := "golang-proxy [PORT] [TARGET]\n" +
+		"Example: golang-proxy 5000 http://localhost:80\n\n" +
+		"PORT:   The port that the proxy should run on.\n" +
+		"TARGET: The target address that the proxy should redirect requests to."
+
+	fmt.Println(text)
+}
+
+func exitError(message string) {
+	printUsage()
+	fmt.Printf("\n---\nError: %v\n", message)
+	os.Exit(1)
+}
+
+func getParams(args []string) (string, string) {
+	var port string
+	var target string
+
+	for i := 1; i < len(args); i++ {
+		switch i {
+		case 1:
+			port = args[i]
+		case 2:
+			target = args[i]
+		}
+	}
+
+	if port == "" {
+		exitError("PORT most be specified")
+	}
+
+	if target == "" {
+		exitError("TARGET mot be specified")
+	}
+
+	return port, target
+}
+
+func printRequestInfo(req *http.Request) {
+	fmt.Printf(
+		"[%v] {%v} %v %v %v\n",
+		req.Method,
+		req.RemoteAddr,
+		req.URL,
+		req.Header["Content-Type"],
+		req.ContentLength,
+	)
+}
+
+func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
+	printRequestInfo(req)
+
+	_, target := getParams(os.Args)
+	url, _ := url.Parse(target)
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy.ServeHTTP(res, req)
+}
+
+func main() {
+	port, target := getParams(os.Args)
+
+	fmt.Println("Server http server...")
+	fmt.Printf("http://localhost:%v --> %v\n", port, target)
+
+	http.HandleFunc("/", handleRequestAndRedirect)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		panic(err)
+	}
+}
